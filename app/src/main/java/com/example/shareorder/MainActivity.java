488430a78a247fa6f5454f219e642c5103301a1d
Gabriela <gabriela.cleims@gmail.com>
2024-01-19 17:47:56 -0300

    first commit

---
+++ app/src/main/java/com/example/shareorder/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.shareorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonShare = findViewById(R.id.button_share);
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Compartilhar entrega");
+                startActivity(Intent.createChooser(shareIntent, "Compartilhar via"));
+            }
+        });
+    }
+}
