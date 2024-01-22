49d3debf1323cd50be682bfa3fb4450bef4839d5
Gabriela <gabriela.cleims@gmail.com>
2024-01-22 17:53:39 -0300

    button ok

--- app/src/main/java/com/example/shareorder/MainActivity.java
+++ app/src/main/java/com/example/shareorder/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonShare = findViewById(R.id.button_share);
+        ImageButton buttonShare1 = findViewById(R.id.button_share1);
         buttonShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent shareIntent = new Intent(Intent.ACTION_SEND);
                 shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, "Compartilhar entrega");
+                String shareText = "Click no link de indicação e ganhe um bônus agora mesmo! " + "https://www.google.com";
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+                startActivity(Intent.createChooser(shareIntent, "Compartilhar via"));
+            }
+        });
+
+        buttonShare1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                String shareText = "Click no link de indicação e ganhe um bônus agora mesmo! " + "https://www.google.com";
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
                 startActivity(Intent.createChooser(shareIntent, "Compartilhar via"));
             }
         });
